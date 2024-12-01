66b36bb4d21652bc552a04651ac3fe99a2ea4918
Shizuka <genshinimpactrucom@gmail.com>
2024-12-01 18:05:36 +0300

    Update PyTest.py

--- PyTest.py
+++ PyTest.py
@@ -1,10 +1,18 @@
 def main(input_str):
     try:
-        num1, operator, num2 = input_str.split()
-        num1, num2 = int(num1), int(num2)
+        try:
+            num1, operator, num2 = input_str.split()
+        except ValueError:
+            raise ValueError("Неправильный формат строки: должно быть 'число оператор число'")
+        
+        try:
+            num1, num2 = int(num1), int(num2)
+        except ValueError:
+            raise ValueError("Одно из введённых значений не является числом")
+        
         if not (1 <= num1 <= 10 and 1 <= num2 <= 10):
             raise ValueError("Числа должны быть от 1 до 10")
-
+        
         if operator == '+':
             return str(num1 + num2)
         elif operator == '-':
@@ -14,7 +22,6 @@ def main(input_str):
         elif operator == '/':
             return str(num1 // num2)
         else:
-            raise ValueError("Неверный оператор")
-    except:
-        return "throws Exception"
-
+            raise ValueError("Неверный оператор: используйте '+', '-', '*', '/'")
+    except ValueError as e:
+        return f"throws Exception: {e}"
